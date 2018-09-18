a5056a55c8a710c716e44dc5cc50c1b67974571e
Lucchetto <lucchetto.tie@live.com>
2018-09-18 21:06:15 +0200

    Revert "Allow using 4G icon instead LTE [1/2]"
    This reverts commit ede159753c535291e93ee1d4a60bfc664f5ec16c.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4288,12 +4288,6 @@ public final class Settings {
         public static final String ROAMING_INDICATOR_ICON = "roaming_indicator_icon";
 
         /**
-         * Whether to display 4G icon instead LTE
-         * @hide
-         */
-        public static final String SHOW_FOURG_ICON = "show_fourg_icon";
-
-        /**
          * @hide
          */
         public static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -77,7 +77,6 @@ public class MobileSignalController extends SignalController<
     private Config mConfig;
 
     private boolean mRoamingIconAllowed;
-    private boolean mShow4gForLte;
 
     // TODO: Reduce number of vars passed in, if we have the NetworkController, probably don't
     // need listener lists anymore.
@@ -128,11 +127,8 @@ public class MobileSignalController extends SignalController<
 
         void observe() {
             ContentResolver resolver = mContext.getContentResolver();
-            resolver.registerContentObserver(
-                    Settings.System.getUriFor(Settings.System.ROAMING_INDICATOR_ICON), false,
-                    this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(
-                    Settings.System.getUriFor(Settings.System.SHOW_FOURG_ICON), false,
+            Uri uri = Settings.System.getUriFor(Settings.System.ROAMING_INDICATOR_ICON);
+            resolver.registerContentObserver(uri, false,
                     this, UserHandle.USER_ALL);
             updateSettings();
         }
@@ -153,11 +149,6 @@ public class MobileSignalController extends SignalController<
                 Settings.System.ROAMING_INDICATOR_ICON, 1,
                 UserHandle.USER_CURRENT) == 1;
 
-        mShow4gForLte = Settings.System.getIntForUser(resolver,
-                Settings.System.SHOW_FOURG_ICON, 0,
-                UserHandle.USER_CURRENT) == 1;
-
-        mapIconSets();
         updateTelephony();
     }
 
@@ -267,7 +258,7 @@ public class MobileSignalController extends SignalController<
         mNetworkToIconLookup.put(TelephonyManager.NETWORK_TYPE_HSPA, hGroup);
         mNetworkToIconLookup.put(TelephonyManager.NETWORK_TYPE_HSPAP, hPlusGroup);
 
-        if (mShow4gForLte) {
+        if (mConfig.show4gForLte) {
             mNetworkToIconLookup.put(TelephonyManager.NETWORK_TYPE_LTE, TelephonyIcons.FOUR_G);
             if (mConfig.hideLtePlus) {
                 mNetworkToIconLookup.put(TelephonyManager.NETWORK_TYPE_LTE_CA,
